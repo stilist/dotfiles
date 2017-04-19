9f9f0d656f177cfb74303cf013b536bab88ed726
Jordan Cole <stilist@ratafia.info>
2017-04-18 19:03:01 -0700

    Automatically export DATABASE_URL

--- shell/.sh/session/postgres.sh
+++ shell/.sh/session/postgres.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
+# @see http://guides.rubyonrails.org/configuring.html#configuring-a-database
+# @see https://www.postgresql.org/docs/9.6/static/libpq-connect.html#AEN45527
+DATABASE_URL=postgres://`whoami`@localhost
+export DATABASE_URL
+
 # Postgres.app - http://postgresapp.com
 PATH="$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"
