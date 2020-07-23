079b06c257d2bdaec36192a0c2f7c893ea0e6318
Jordan Cole <33663+stilist@users.noreply.github.com>
2020-07-23 10:32:22 -0700

    Adapt lsl for GNU ls

--- dot_sh/session/ls.sh
+++ dot_sh/session/ls.sh
@@ -5,4 +5,8 @@
 # l: long
 # p: slash after directories
 # T: full timestamp
+{{- if eq .chezmoi.os "darwin" }}
 alias lsl='ls -AGlpT'
+{{ else if eq .chezmoi.os "linux" }}
+alias lsl='ls --almost-all --color=always -l --indicator-style=slash --time-style="+%FT%T%z"'
+{{ end }}
