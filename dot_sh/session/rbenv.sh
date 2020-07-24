1cfc4f2d59e18ef66458c024e05a8606551a730d
Jordan Cole <33663+stilist@users.noreply.github.com>
2020-07-24 12:57:28 -0700

    Configure OpenSSL version for Ruby compilation
    Based on instructions from ruby-build.

--- dot_sh/session/rbenv.sh
+++ dot_sh/session/rbenv.sh
@@ -11,4 +11,11 @@ fi
 
 if [ -d "${HOME}/.rbenv/plugins/ruby-build" ] ; then
   PATH="${PATH}:${HOME}/.rbenv/plugins/ruby-build/bin"
+
+  if which brew >/dev/null 2>&1 ; then
+    if brew --prefix openssl@1.1 >/dev/null 2>&1 ; then
+      RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+      export RUBY_CONFIGURE_OPTS
+    fi
+  fi
 fi
