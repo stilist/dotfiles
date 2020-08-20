58f56da1bdcf3504e5d2da5e320c8c614a48d2ea
Jordan Cole <33663+stilist@users.noreply.github.com>
2020-08-19 19:41:15 -0700

    Fix remaining warnings

--- dot_sh/session/postgres.sh
+++ dot_sh/session/postgres.sh
@@ -2,7 +2,7 @@
 
 # @see http://guides.rubyonrails.org/configuring.html#configuring-a-database
 # @see https://www.postgresql.org/docs/9.6/static/libpq-connect.html#AEN45527
-DATABASE_URL=postgres://`whoami`@localhost
+DATABASE_URL="postgres://$(whoami)@localhost"
 export DATABASE_URL
 
 # Postgres.app - http://postgresapp.com
