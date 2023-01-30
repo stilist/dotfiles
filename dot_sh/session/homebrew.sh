807fc2d9f55c589f3cb938fe3583854d355423cb
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-01-30 10:14:01 -0800

    Ensure Homebrew is on $PATH for Apple chips

---
+++ dot_sh/session/homebrew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Homebrew's installation path differs by CPU architecture on macOS. This is
+# adapted from the Homebrew installation script.
+#
+# @note On Intel Macs (`x86_64`) Homebrew is installed to `/usr/local/bin`,
+#   which is part of the default `$PATH`.
+# @see https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+if [[ "$(/usr/bin/uname -m)" == "arm64" ]] ; then
+  PATH="${PATH}:/opt/homebrew"
+fi
