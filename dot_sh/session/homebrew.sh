0f7f68c58579f39c85fb96fba37521a38d93db8a
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-01-30 13:02:30 -0800

    Prepend path instead of append

--- dot_sh/session/homebrew.sh
+++ dot_sh/session/homebrew.sh
@@ -7,5 +7,7 @@
 #   which is part of the default `$PATH`.
 # @see https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 if [[ "$(/usr/bin/uname -m)" == "arm64" ]] ; then
-  PATH="${PATH}:/opt/homebrew/bin"
+  # Insert at *start* of `$PATH` so Homebrew-managed software takes precedence
+  # over system-managed software.
+  PATH="/opt/homebrew/bin:${PATH}"
 fi
