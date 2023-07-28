741471aac8da7cc5486a1b15c3f482b6839fe7fc
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-07-27 18:46:08 -0700

    Fix pyenv issue

--- dot_sh/session/python.sh
+++ dot_sh/session/python.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
-# `pip install --user` will put binaries in this path. (`--user` avoids
-# the need to use `sudo`).
 if command -v python >/dev/null 2>&1 ; then
-  PATH="${PATH}:$(python -c 'import site; print(site.USER_BASE)')/bin"
+  # Handle case that `pyenv` has a `python` shim, but the shim can't resolve
+  # a path for `python`.
+  if python -c '1' >/dev/null 2>&1 ; then
+    # `pip install --user` will put binaries in this path. (`--user` avoids
+    # the need to use `sudo`).
+    PATH="${PATH}:$(python -c 'import site; print(site.USER_BASE)')/bin"
+  fi
 fi
