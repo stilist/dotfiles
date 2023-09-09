cbc13dde310416ea14d4a6e3f545799f44fd9e0c
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-09-09 09:31:58 -0700

    Set up rbenv in .rc

---
+++ dot_sh/rc/rubygems.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+if command -v gem >/dev/null 2>&1 ; then
+  # The paths `gem environment gempath` returns don't include `/bin`, which
+  # is where the binaries are -- what matters for `PATH`.
+  gempaths="$(gem environment gempath | sed -e "s/:/\/bin:/g")"
+  PATH="$PATH:$gempaths/bin"
+fi
