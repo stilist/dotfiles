937de51b5dab232dec8ab40e94e06e6f42985da8
Jordan Cole <33663+stilist@users.noreply.github.com>
2025-06-25 14:37:26 -0700

    Fix asdf script path

--- dot_sh/rc/executable_asdf.sh
+++ dot_sh/rc/executable_asdf.sh
@@ -3,6 +3,6 @@
 # asdf - https://asdf-vm.com
 if command -v asdf >/dev/null 2>&1 ; then
   if command -v brew >/dev/null 2>&1 ; then
-    . "$(brew --prefix asdf)"/asdf/libexec/asdf.sh
+    . "$(brew --prefix asdf)"/libexec/asdf.sh
   fi
 fi
